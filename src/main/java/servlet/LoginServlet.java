a7aaf65dfcd55e60b1f704242a8da2c324c673d3
kobeaibiancheng <wyx160413@yeah.net>
2020-07-30 16:04:15 +0800

    对LoginServlet进行修改

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import entity.Account;
+import service.AccountService;
 import util.DBUtil;
 
 import javax.servlet.ServletException;
@@ -27,58 +28,31 @@ public class LoginServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        AccountService accountService = new AccountService();
+        Account accountLogin = new Account();
 
-        Writer writer = resp.getWriter();
+        accountLogin.setUsername(username);
+        accountLogin.setPassword(password);
 
-        try{
-            String sql = "select id,username,password from account where username = ? and password = ?";
-            connection = DBUtil.getConnection(true);//获得数据库连接
-            ps = connection.prepareStatement(sql);
+        System.out.println(accountLogin);
 
-            //给两个占位符 ？  赋值
-            ps.setString(1,username);
-            ps.setString(2,password);
-
-            rs = ps.executeQuery();//查询   返回结果集
-
-
-            Account user = new Account();
-
-            if (rs.next()) {
-                Integer id = rs.getInt("id");//拿到 id 列的数据
-                user.setId(id);
-                user.setUsername(rs.getString("username"));
-                user.setPassword(rs.getString("password"));
-            }
-
-            //判断是否有这个用户
-            if (user.getId() == null) {
-                writer.write("<h2> 没有该用户：" + username+ "</h2>");
-                resp.sendRedirect("register.html");
-            }else if (!password.equals(user.getPassword())) {
-                writer.write("<h2> 密码错误：" + username+ "</h2>");
-                resp.sendRedirect("login.html");
-            }else {
-
+        try {
+            Account account = accountService.login(accountLogin);
+            System.out.println(account);
+            if (account != null) {
                 //session   保存用户信息 用来后续的功能知道是哪个用户
                 HttpSession session = req.getSession();
-                session.setAttribute("user",user);
-                writer.write("<h2> 登陆成功：" + username+ "</h2>");
-                System.out.println("登陆成功");
+                session.setAttribute("account",account);
                 resp.sendRedirect("index.html");
+                System.out.println("登陆成功: " + account);
+            }else {
+                resp.sendRedirect("login.html");
             }
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
-            try {
-                DBUtil.close(connection,ps,rs);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
+
+
 
     }
 }
