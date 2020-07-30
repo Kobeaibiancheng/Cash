feed910292043300c6218aa091f6be0d1256c086
kobeaibiancheng <wyx160413@yeah.net>
2020-07-30 22:45:34 +0800

    AccountService

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -2,8 +2,10 @@ package service;
 
 import dao.AccountDao;
 import entity.Account;
+import entity.Goods;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class AccountService {
     /**
@@ -18,9 +20,32 @@ public class AccountService {
         return account;
     }
 
+    /**
+     * 注册
+     * @param registerAccount
+     * @return
+     * @throws SQLException
+     */
     public int register(Account registerAccount) throws SQLException {
         AccountDao accountDao = new AccountDao();
         int ret = accountDao.register(registerAccount);
+        return ret;
+    }
+
+    /**
+     * 浏览商品
+     * @return
+     * @throws SQLException
+     */
+    public List<Goods> goodsBrowse() throws SQLException {
+        AccountDao accountDao = new AccountDao();
+        List<Goods> listGoods = accountDao.goodsBrowse();
+        return listGoods;
+    }
+
+    public int goodsPutAway(Goods addGoods) throws SQLException {
+        AccountDao accountDao = new AccountDao();
+        int ret = accountDao.goodsPutAway(addGoods);
         return ret;
     }
 }
